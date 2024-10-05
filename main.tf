4229a1b52139f3a9f64507488ce83673dec30cdd
deepak <test@test>
2024-10-05 11:01:54 +0530

    terraform apply

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
     
   }
   subscription_id = "15dde903-3c88-40b6-b69a-17c6e5f1dc76"
-  tenant_id = "a3efabbf-75bc-4eb0-8cd6-6fbeace4d4cf"
+  tenant_id = "a3efabbf-75bc-4eb0-8cd6-6fbeace4d4cfs"
 }
 
 resource "azurerm_resource_group" "rg1test" {
